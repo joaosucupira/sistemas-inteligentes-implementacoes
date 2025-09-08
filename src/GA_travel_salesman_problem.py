c4b7b148dc003062c3b0f9ff75a78846fa00ef34
João Sucupira <jasucupira@gmail.com>
2025-09-08 16:51:04 -0300

    aplicando elitismo

--- src/GA_travel_salesman_problem.py
+++ src/GA_travel_salesman_problem.py
@@ -4,20 +4,20 @@ from random import randint, random, choice, shuffle, uniform
 from string import ascii_uppercase
 
 INF = maxsize
-GRAPH = 20
-WEIGHT_RANGE = (1,10)
-MUTATION = 0.02
-START = 'A'
-POPULATION = 100
+GRAPH = 5
+WEIGHT_RANGE = (1,100)
+MUTATION = 0.05
+START = 0
+POPULATION = 4
 INDIVIDUAL = GRAPH - 1
-GENERATIONS = 500
+GENERATIONS = 10000
 PRINT_INTERVAL = 100
-
+TESTS = 5
 class Graph:
     def __init__(self, size=GRAPH, weight_range=WEIGHT_RANGE):
         self.matrix = [[0] * size for _ in range(size)]
         self.size = size
-        self.nodes = list(ascii_uppercase[:size])
+        self.nodes = list(range(size))
         self.fill_weights(weight_range)
         
     def __str__(self):
@@ -43,7 +43,6 @@ class Graph:
         for i, weight in enumerate(self.matrix[node_index]):
             if weight != 0:
                 neighbors.append(self.nodes[i])
-                    
         return neighbors
     
     def get_weight(self, n1, n2):
@@ -60,12 +59,12 @@ class TSP:
         self.generations = []
         self.history = []
         self.greedy_cost = None
- 
-    
+
     def get_random_individual(self):
-        labels = self.graph.get_node_neighbors(self.start_node)
+        labels = list(self.graph.nodes)
+        labels.remove(self.start_node)
         shuffle(labels)
-        individual = "".join(labels)
+        individual = labels
         return individual
             
     def get_initial_population(self):
@@ -77,60 +76,48 @@ class TSP:
         return pop
 
     def print_generations(self, interval=PRINT_INTERVAL):
-        for i in range(0, len(self.generations)):
+        for i in range(len(self.generations)):
             if (i % interval == 0):
                 print(f'gen {i} best fit = {min(self.get_fitness(ind) for ind in self.generations[i])}')
     
     def get_fitness(self, individual):
-        # TODO: Calculate fitness function from path string
         fitness_score = 0
-        start = self.start_node
-        G = self.graph
-        path = start + individual + start
+        path = [self.start_node] + individual + [self.start_node]
         
-        for i in range(len(path)- 1):
+        for i in range(len(path) - 1):
             current_node = path[i]
             next_node = path[i+1]
-            
-            fitness_score += G.get_weight(current_node, next_node)
-        
+            fitness_score += self.graph.get_weight(current_node, next_node)
         return fitness_score
     
     def get_fitness_sum(self, population):
-        return sum(self.get_fitness(individual) for individual in population)
+        return sum(self.get_fitness(ind) for ind in population)
     
     def get_selected_parent(self, population):
+        # optmizing selection by storing just once the list of fitnes
+        fitness_list = [self.get_fitness(ind) for ind in population]
+        worst_fit = max(fitness_list) + 1
         
-        # getting the worst fit from the population
-        worst_fit = max(self.get_fitness(ind) for ind in population) + 1
-        total_aptitude = sum(worst_fit - self.get_fitness(ind) for ind in population)
-        
-        # spinning the wheel for a random value under the total fitness sum up
+        aptitude_list = [worst_fit - fit for fit in fitness_list]
+        total_aptitude = sum(aptitude_list)
+            
         spin = uniform(0, total_aptitude)
-        s = 0
+        current_sum = 0
         
-        for individual in population:
+        for i, individual in enumerate(population):
+            current_sum += aptitude_list[i]
             
-            # accumulates the sum and compares it to the gap between the worst fit and the current genes fitness
-            fitness = worst_fit - self.get_fitness(individual)
-            s += fitness
-            
-            # choosing wheel gets most probably the individuals with LESSER total cost
-            if s >= spin:
+            if current_sum >= spin:
                 return individual
-            
-        # rarely, it will choose none, so just pick a random one/ Fallback
+        
         return choice(population)
     
     def get_crossover(self, p1, p2):
         length = len(p1)
         cross_point = randint(1, length - 1)
         
-        # order crossover
-        # we use order crossover so we dont repeat/lose nodes for the solution
-        # we get part of a chromossome and filter the second part with the ones missing according to the order not the exact sub string
-        child1 = p1[:cross_point] + "".join(char for char in p2 if char not in p1[:cross_point])
-        child2 = p2[:cross_point] + "".join(char for char in p1 if char not in p2[:cross_point])
+        child1 = p1[:cross_point] + [char for char in p2 if char not in p1[:cross_point]]
+        child2 = p2[:cross_point] + [char for char in p1 if char not in p2[:cross_point]]
         
         return child1, child2
 
@@ -145,9 +132,8 @@ class TSP:
                 new_gen.append(c1)
                 if (len(new_gen) != n):
                     new_gen.append(c2)
-    
         return new_gen
-          
+        
     def mutation(self, individual):
         if random() >= MUTATION:
             return individual
@@ -159,18 +145,16 @@ class TSP:
         
         trade = list(individual)
         trade[m1], trade[m2] = trade[m2], trade[m1]
-        return "".join(trade)
+        return trade
 
     def put_new_generation(self):
-        new_gen = self.get_select_and_crossover(self.generations[-1])  
-        for i in range(0, len(new_gen)):
+        new_gen = self.get_select_and_crossover(self.generations[-1])
+        for i in range(len(new_gen)):
             new_gen[i] = self.mutation(new_gen[i])
-                
         self.generations.append(new_gen)
+ 
     def plot_performance(self):
-    
         greedy_performance = [self.greedy_cost] * (GENERATIONS + 1)
-        
         plt.figure(figsize=(12, 8))
         plt.plot(range(len(self.history)), self.history, marker='o', linestyle='-', label='Algoritmo Genético', color='blue')
         plt.plot(range(len(greedy_performance)), greedy_performance, linestyle='--', color='red', label='Algoritmo Guloso')
@@ -179,7 +163,9 @@ class TSP:
         plt.ylabel('Melhor Fitness (Distância Total)')
         plt.grid(True)
         plt.legend()
-        plt.show()
+        # plt.show()
+        plt.waitforbuttonpress()
+        plt.close()
 
     def execute(self):
         greedy_solver = TSPGreedy(self.graph)
@@ -188,15 +174,12 @@ class TSP:
         
         self.generations.append(self.get_initial_population())
         self.history.append(min(self.get_fitness(ind) for ind in self.generations[-1]))
-        for _ in range(0, GENERATIONS - 1):
+        for _ in range(GENERATIONS - 1):
             self.put_new_generation()
             self.history.append(min(self.get_fitness(ind) for ind in self.generations[-1]))
-            
-
-        # print("Generations evolution")
-        # self.print_generations()
+        
         self.plot_performance()
-
+        
 class TSPGreedy:
     def __init__(self, graph):
         self.graph = graph
@@ -207,14 +190,14 @@ class TSPGreedy:
         current_node_label = start_node_label
         
         while len(visited_nodes) < self.graph.size:
-            current_node_index = self.graph.nodes.index(current_node_label)
             min_weight = INF
             next_node_label = None
 
-            for i, weight in enumerate(self.graph.matrix[current_node_index]):
+            for i in range(self.graph.size):
                 neighbor_label = self.graph.nodes[i]
-                if neighbor_label not in visited_nodes and weight < min_weight:
-                    min_weight = weight
+                current_node_index = self.graph.nodes.index(current_node_label)
+                if neighbor_label not in visited_nodes and self.graph.get_weight(current_node_index, i) < min_weight:
+                    min_weight = self.graph.get_weight(current_node_index, i)
                     next_node_label = neighbor_label
 
             if next_node_label:
@@ -229,21 +212,18 @@ class TSPGreedy:
         return path
 
     def get_path_cost(self, path):
-            cost = 0
-            for i in range(len(path) - 1):
-
-                n1_label = path[i]
-                n2_label = path[i+1]
-                cost += self.graph.get_weight(n1_label, n2_label)
-            return cost
-
+        cost = 0
+        for i in range(len(path) - 1):
+            n1_label = path[i]
+            n2_label = path[i+1]
+            cost += self.graph.get_weight(n1_label, n2_label)
+        return cost
         
 def main():
     G = Graph(GRAPH)
-    travel_salesman_problem = TSP(G)
-    travel_salesman_problem.execute()
+    for i in range(0, TESTS):
+        travel_salesman_problem = TSP(G)
+        travel_salesman_problem.execute()
 
-    
 if __name__ == '__main__':
     main()
-        
